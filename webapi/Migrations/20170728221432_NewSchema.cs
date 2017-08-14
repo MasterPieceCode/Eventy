22cbb98fd40c47af8a8ee0b85df463a2d6667f0e
H113228 <Eric.Gasanov@halliburton.com>
2017-08-14 17:58:14 -0500

    styles added

---
+++ webapi/Migrations/20170728221432_NewSchema.cs
@@ -0,0 +1,389 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace webapi.Migrations
+{
+    public partial class NewSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cards_Cards_EventId",
+                table: "Cards");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefData_ApplicationUser_UserId1",
+                table: "RefData");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ApplicationUser",
+                table: "ApplicationUser");
+
+            migrationBuilder.DropColumn(
+                name: "EventId",
+                table: "Cards");
+
+            migrationBuilder.DropColumn(
+                name: "CreateDate",
+                table: "ApplicationUser");
+
+            migrationBuilder.DropColumn(
+                name: "LastPasswordChangeDate",
+                table: "ApplicationUser");
+
+            migrationBuilder.DropColumn(
+                name: "LastSignInDate",
+                table: "ApplicationUser");
+
+            migrationBuilder.RenameTable(
+                name: "ApplicationUser",
+                newName: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "Lttitude",
+                table: "Cards",
+                newName: "Location_Lttitude");
+
+            migrationBuilder.RenameColumn(
+                name: "Longitude",
+                table: "Cards",
+                newName: "Location_Longitude");
+
+            migrationBuilder.RenameColumn(
+                name: "CountryId",
+                table: "Cards",
+                newName: "Location_CountryId");
+
+            migrationBuilder.RenameColumn(
+                name: "CityId",
+                table: "Cards",
+                newName: "Location_CityId");
+
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "Cards",
+                newName: "Location_Address");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AspNetUsers",
+                table: "AspNetUsers",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "AspNetRoles",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    ConcurrencyStamp = table.Column<string>(type: "TEXT", nullable: true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 256, nullable: true),
+                    NormalizedName = table.Column<string>(type: "TEXT", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ClaimType = table.Column<string>(type: "TEXT", nullable: true),
+                    ClaimValue = table.Column<string>(type: "TEXT", nullable: true),
+                    UserId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserClaims_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserLogins",
+                columns: table => new
+                {
+                    LoginProvider = table.Column<string>(type: "TEXT", nullable: false),
+                    ProviderKey = table.Column<string>(type: "TEXT", nullable: false),
+                    ProviderDisplayName = table.Column<string>(type: "TEXT", nullable: true),
+                    UserId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserLogins", x => new { x.LoginProvider, x.ProviderKey });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserLogins_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserTokens",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "TEXT", nullable: false),
+                    LoginProvider = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Value = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserTokens", x => new { x.UserId, x.LoginProvider, x.Name });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetRoleClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ClaimType = table.Column<string>(type: "TEXT", nullable: true),
+                    ClaimValue = table.Column<string>(type: "TEXT", nullable: true),
+                    RoleId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoleClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetRoleClaims_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserRoles",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "TEXT", nullable: false),
+                    RoleId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserRoles", x => new { x.UserId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_Location_CityId",
+                table: "Cards",
+                column: "Location_CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_Location_CountryId",
+                table: "Cards",
+                column: "Location_CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers",
+                column: "NormalizedEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoleClaims_RoleId",
+                table: "AspNetRoleClaims",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserClaims_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserLogins_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_RoleId",
+                table: "AspNetUserRoles",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cards_RefData_Location_CityId",
+                table: "Cards",
+                column: "Location_CityId",
+                principalTable: "RefData",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cards_RefData_Location_CountryId",
+                table: "Cards",
+                column: "Location_CountryId",
+                principalTable: "RefData",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefData_AspNetUsers_UserId1",
+                table: "RefData",
+                column: "UserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cards_RefData_Location_CityId",
+                table: "Cards");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cards_RefData_Location_CountryId",
+                table: "Cards");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefData_AspNetUsers_UserId1",
+                table: "RefData");
+
+            migrationBuilder.DropTable(
+                name: "AspNetRoleClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserLogins");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "AspNetRoles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cards_Location_CityId",
+                table: "Cards");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cards_Location_CountryId",
+                table: "Cards");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AspNetUsers",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUsers",
+                newName: "ApplicationUser");
+
+            migrationBuilder.RenameColumn(
+                name: "Location_Lttitude",
+                table: "Cards",
+                newName: "Lttitude");
+
+            migrationBuilder.RenameColumn(
+                name: "Location_Longitude",
+                table: "Cards",
+                newName: "Longitude");
+
+            migrationBuilder.RenameColumn(
+                name: "Location_CountryId",
+                table: "Cards",
+                newName: "CountryId");
+
+            migrationBuilder.RenameColumn(
+                name: "Location_CityId",
+                table: "Cards",
+                newName: "CityId");
+
+            migrationBuilder.RenameColumn(
+                name: "Location_Address",
+                table: "Cards",
+                newName: "Address");
+
+            migrationBuilder.AddColumn<long>(
+                name: "EventId",
+                table: "Cards",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreateDate",
+                table: "ApplicationUser",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastPasswordChangeDate",
+                table: "ApplicationUser",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastSignInDate",
+                table: "ApplicationUser",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ApplicationUser",
+                table: "ApplicationUser",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cards_Cards_EventId",
+                table: "Cards",
+                column: "EventId",
+                principalTable: "Cards",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefData_ApplicationUser_UserId1",
+                table: "RefData",
+                column: "UserId1",
+                principalTable: "ApplicationUser",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
